120bf6ec83a93c28ac1e3202d206240ed28edd66
sabhaskar83 <70048507+sabhaskar83@users.noreply.github.com>
2021-02-11 15:42:04 +0530

    Added Controller,Model, Repository claass

---
+++ Models/ServiceRequestContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceRequest.Models
+{
+    public class ServiceRequestContext:DbContext
+    {
+        public ServiceRequestContext(DbContextOptions options):base(options)
+        {
+
+        }
+    }
+}
---
+++ Models/ServiceRequestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceRequest.Models
+{
+    [Table("servicerequest")]
+    public class ServiceRequestModel
+    {
+        public Guid id { get; set; }
+        public string buildingCode { get; set; }
+        public string description { get; set; }
+        public int currentStatus { get; set; }
+        public string createdBy { get; set; }
+        public DateTime createdDate { get; set; }
+        public string lastModifiedBy { get; set; }
+        public DateTime lastModifiedDate { get; set; }
+
+    }
+}
